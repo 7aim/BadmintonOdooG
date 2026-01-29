cf7b04d43eb312c92b43034d88f14c18ac4d5098
Aim <7asim6@gmail.com>
2026-01-29 11:05:03 +0000

    Paket filter is deleted and deleted qr scaner message little is changed.

--- models/qr_scanner.py
+++ models/qr_scanner.py
@@ -79,7 +79,7 @@ class QRScannerWizard(models.TransientModel):
                     'duration_hours': 1.0,
                 })
 
-                self.result_message = f"✅ SESSİYA YARADILDI (Gözləmədə)!\n👤 Müştəri: {partner.name}\n🎮 Sessiya: {session.name}\n⚠️ Zəhmət olmasa 'Başlat' düyməsinə basın!\n💰 Balans: {current_balance} saat\n 🔢 Növbə: {session.queue_number}"
+                self.result_message = f"✅ SESSİYA YARADILDI (Gözləmədə)!\n👤 Müştəri: {partner.name}\n💰 Balans: {current_balance} saat\n 🔢 Növbə: {session.queue_number}"
                 self.session_id = session.id
                 
                 return self._return_wizard()
